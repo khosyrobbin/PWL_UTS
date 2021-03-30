6c194c1d15d555e95f805987fadb246229986a4c
khosy robbin <khosybin@gmail.com>
2021-03-30 19:35:06 +0700

    add index for views

---
+++ resources/views/index.blade.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+	<title>Barang UTS</title>
+</head>
+<body>
+ 
+	<style type="text/css">
+		.pagination li{
+			float: left;
+			list-style-type: none;
+			margin:5px;
+		}
+	</style>
+ 
+	
+	<h3>Data Barang</h3>
+ 
+        
+
+	<table border="1">
+		<tr>
+			<th>Id Barang</th>
+            <th>Kode Barang</th>
+			<th>Nama Barang</th>
+			<th>Kategori</th>
+			<th>Harga</th>
+            <th>QTY</th>
+		</tr>
+		@foreach($barangs as $p)
+		<tr>
+			<td>{{ $p->id_barang }}</td>
+			<td>{{ $p->kode_barang }}</td>
+			<td>{{ $p->nama_barang }}</td>
+			<td>{{ $p->kategori_barang }}</td>
+            <td>{{ $p->harga }}</td>
+            <td>{{ $p->qty }}</td>
+		</tr>
+		@endforeach
+	</table>
+ 
+	<br/>
+	Halaman : {{ $barangs->currentPage() }} <br/>
+	Jumlah Data : {{ $barangs->total() }} <br/>
+	Data Per Halaman : {{ $barangs->perPage() }} <br/>
+ 
+ 
+	{{ $barangs->links() }}
+ 
+ 
+</body>
+</html>
