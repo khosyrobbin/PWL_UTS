1f76d2c27605972a5d7ce8172ca77ee4d8fc2f80
khosy robbin <khosybin@gmail.com>
2021-03-30 19:30:02 +0700

    input code at model

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    use HasFactory;
-}
+    protected $table="barangs";
+    public $timestamps= false;
+    protected $primarykey = 'id_barang';
+    protected $fillable= [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty'
+    ];
+};
